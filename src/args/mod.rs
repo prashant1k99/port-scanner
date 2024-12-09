e761f086fdb3808591ecb4bb149d71cdc9e0fa86
Prashant <prashantco111@gmail.com>
2024-12-09 19:13:33 +0530

    Handled help flag as well

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -21,6 +21,20 @@ impl Arguments {
         if args.len() < 2 {
             return Err("Insufficient Arguments passed");
         }
+
+        let help_flags = ["--help", "-h"];
+        if let Some(arg) = args.get(1) {
+            if help_flags.contains(&arg.as_str()) {
+                return Err("Help flag passed");
+            }
+        }
+
+        if !IpAddr::V4(args[1].parse().unwrap()).is_ipv4()
+            && !IpAddr::V6(args[1].parse().unwrap()).is_ipv6()
+        {
+            println!("This is neither IPv4 or Ipv6")
+        }
+
         Ok(Arguments {
             host: IpAddr::V4(Ipv4Addr::new(127, 1, 1, 1)),
             flags: None,
