dc9b4c850cd665ea329e337fcdc2fbe1cbe3e97f
Prashant <prashantco111@gmail.com>
2024-12-10 16:25:26 +0530

    Added IPv6 test cases

--- src/args/tests.rs
+++ src/args/tests.rs
@@ -43,6 +43,12 @@ mod arguments_test {
                 is_sweep: false
             })
         );
+
+        let result = Arguments::new(&[
+            String::from("cli-app"),
+            String::from("2001:0db8:85a3:0000:0000:8a2e:0370:7334"),
+        ]);
+        assert!(result.is_ok());
     }
 
     #[test]
